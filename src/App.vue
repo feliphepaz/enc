bfe84535c6c7ae64c27dcb08ed593fa920dff689
feliphepaz <feliphe.s.paz@gmail.com>
2025-08-13 20:53:14 -0300

    feat: add home

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <Header />
+  <Home />
 </template>
 
 <script setup>
 import Header from "./views/Header.vue";
+import Home from "./views/Home.vue";
 </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="home">
+    <img
+      class="home__logo-promo"
+      src="../assets/home/logo-promo.webp"
+      alt="Logo da promoção"
+    />
+  </section>
+</template>
+
+<style scoped lang="scss">
+.home {
+  background: url("../assets/home/bg-home.webp") no-repeat center center;
+  margin-top: $header-height-desktop;
+  height: 648px;
+
+  &__logo-promo {
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    max-width: 700px;
+    animation: moveUpDown 2s ease-in-out infinite;
+  }
+
+  @include mobile {
+    background: url("../assets/home/bg-home-mob.webp") no-repeat left bottom;
+    margin-top: 0;
+    height: 800px;
+
+    &__logo-promo {
+      top: 40%;
+    }
+  }
+}
+
+@keyframes moveUpDown {
+  50% {
+    transform: translate(-50%, -53%);
+  }
+  100% {
+    transform: translate(-50%, -50%);
+  }
+}
+</style>
