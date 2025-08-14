fb87cf8081503ffb76e5d13de8e1ee5bcbaddabc
feliphepaz <feliphe.s.paz@gmail.com>
2025-08-13 21:32:34 -0300

    feat: insert call to action section in app vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <Header />
   <Home />
+  <CallToAction />
 </template>
 
 <script setup>
 import Header from "./views/Header.vue";
 import Home from "./views/Home.vue";
+import CallToAction from "./views/CallToAction.vue";
 </script>
