07e0002b1b05d050bce1104e5cc4a384fd233930
feliphepaz <feliphe.s.paz@gmail.com>
2025-08-13 21:31:30 -0300

    feat: add call to action

---
+++ src/components/Button.vue
@@ -0,0 +1,44 @@
+<template>
+  <a class="button" :href="link">{{ title }}</a>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.button {
+  background: $yellow;
+  color: $blue;
+  font-family: $font-expanded;
+  text-transform: uppercase;
+  font-size: 24px;
+  line-height: 28px;
+  letter-spacing: 1px;
+  padding: 16px 32px;
+  border-radius: 30px;
+  box-shadow: 2px 8px 8px rgba(0, 0, 0, 0.2);
+  transition: 0.2s;
+
+  &:hover {
+    background: $blue;
+    color: $yellow;
+  }
+
+  @include mobile {
+    font-size: 20px;
+    line-height: 24px;
+  }
+}
+</style>
---
+++ src/views/CallToAction.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="call-to-action">
+    <h1 class="call-to-action__title">
+      Participe do <strong>desafio Hellmann's Loot Irresistível</strong> e
+      concorra a prêmios especiais
+    </h1>
+
+    <Button title="Participe" link="#" />
+  </section>
+</template>
+
+<script setup>
+import Button from "@/components/Button.vue";
+</script>
+
+<style scoped lang="scss">
+.call-to-action {
+  background: #ffffff;
+  text-align: center;
+  padding: 40px 20px;
+
+  &__title {
+    font-family: $font-expanded;
+    color: $blue;
+    text-transform: uppercase;
+    font-size: 36px;
+    line-height: 42px;
+    max-width: 1180px;
+    margin: 0 auto 20px auto;
+    text-align: center;
+
+    strong {
+      color: $dark-blue;
+    }
+  }
+
+  @include mobile {
+    &__title {
+      font-size: 28px;
+      line-height: 32px;
+    }
+  }
+}
+</style>
