7fde05eafee74230aa37f47109288677077e0305
feliphepaz <feliphe.s.paz@gmail.com>
2025-08-14 10:57:36 -0300

    feat: add awards

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <Header />
   <Home />
   <CallToAction />
+  <Awards />
 </template>
 
 <script setup>
 import Header from "./views/Header.vue";
 import Home from "./views/Home.vue";
 import CallToAction from "./views/CallToAction.vue";
+import Awards from "./views/Awards.vue";
 </script>
---
+++ src/views/Awards.vue
@@ -0,0 +1,116 @@
+<template>
+  <section class="awards">
+    <img
+      src="../assets/awards/premios-balao-1.webp"
+      alt="Balão da seção de prêmios"
+      class="awards__balloon -left"
+    />
+
+    <div class="container">
+      <img
+        class="awards__title -desktop"
+        src="../assets/awards/titulo-premios.webp"
+        alt="Título da seção de prêmios"
+      />
+      <img
+        class="awards__title -mobile"
+        src="../assets/awards/titulo-premios-mob.webp"
+        alt="Título da seção de prêmios"
+      />
+
+      <div class="awards__items">
+        <div class="awards__items__item">
+          <img src="../assets/awards/premios-1.webp" alt="Prêmio 1" />
+        </div>
+
+        <div class="awards__items__item">
+          <img src="../assets/awards/premios-2.webp" alt="Prêmio 2" />
+        </div>
+
+        <div class="awards__items__item">
+          <img src="../assets/awards/premios-3.webp" alt="Prêmio 3" />
+        </div>
+
+        <div class="awards__items__item">
+          <img src="../assets/awards/premios-4.webp" alt="Prêmio 4" />
+        </div>
+      </div>
+    </div>
+
+    <img
+      src="../assets/awards/premios-balao-2.webp"
+      alt="Balão da seção de prêmios"
+      class="awards__balloon -right"
+    />
+  </section>
+</template>
+
+<style scoped lang="scss">
+.awards {
+  background: url("../assets/awards/bg-premios.webp") no-repeat center center;
+  background-size: cover;
+  padding: 40px 0;
+  position: relative;
+
+  &__balloon {
+    position: absolute;
+    z-index: 9;
+
+    &.-left {
+      top: -120px;
+      left: 0;
+    }
+
+    &.-right {
+      bottom: 10px;
+      right: 10px;
+    }
+  }
+
+  &__title {
+    max-width: 100%;
+    height: auto;
+    display: block;
+    margin: 0 auto 20px auto;
+
+    &.-mobile {
+      display: none;
+    }
+  }
+
+  &__items {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+
+    &__item {
+      img {
+        width: 100%;
+        height: auto;
+        display: block;
+      }
+    }
+  }
+
+  @include mobile {
+    &__balloon {
+      display: none;
+    }
+
+    &__title {
+      margin: 0 auto 40px auto;
+
+      &.-desktop {
+        display: none;
+      }
+      &.-mobile {
+        display: block;
+      }
+    }
+
+    &__items {
+      grid-template-columns: 1fr;
+    }
+  }
+}
+</style>
